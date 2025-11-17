06c5f11d680a6d5a235f2643dc7a4f82e8cebb5d
apbmsea <apbmsea@gmail.com>
2025-11-17 15:30:28 +0500

    add navigate

--- services/user-service/client/src/features/auth/register/model/registerSaga.ts
+++ services/user-service/client/src/features/auth/register/model/registerSaga.ts
@@ -8,11 +8,13 @@ import { register } from '../api/register.api';
 import type { RegisterPayload } from './register.types';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { isHandledError } from '@shared/utils/isHandledError';
+import { navigateTo } from '@shared/utils/navigate';
 
 function* registerSaga(action: PayloadAction<RegisterPayload>) {
 	try {
 		yield* call(register, action.payload);
 		yield* put(registerSuccess());
+		yield* call(navigateTo, 'login');
 	} catch (error: unknown) {
 		if (isHandledError(error)) {
 			yield* put(registerFailure(error.data.errors));
