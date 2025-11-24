a55a7017b32eb473bb095347a496fc40596e42bb
p4eJlov0d <bkmz1d@inbox.ru>
2025-11-24 19:31:28 +0500

    init postboy module
    The module was initialized: Redis container and RabbitMQ were added, also spring project was initialized

---
+++ services/notification-service/server/module/postboy-module/src/main/java/ru/p4ejlov0d/PostboyModuleApplication.java
@@ -0,0 +1,13 @@
+package ru.p4ejlov0d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostboyModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostboyModuleApplication.class, args);
+    }
+
+}
