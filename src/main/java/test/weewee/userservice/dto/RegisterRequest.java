ccc554f1a220830edc4f847d826de855b97adb8e
weewee231 <wee224225@gmail.com>
2025-12-05 17:04:08 +0500

    changed validation

--- src/main/java/test/weewee/userservice/dto/RegisterRequest.java
+++ src/main/java/test/weewee/userservice/dto/RegisterRequest.java
@@ -1,9 +1,10 @@
 package test.weewee.userservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import test.weewee.userservice.validation.ValidEmail;
+import test.weewee.userservice.validation.ValidFirstName;
+import test.weewee.userservice.validation.ValidLastName;
 import test.weewee.userservice.validation.ValidPassword;
 import test.weewee.userservice.validation.ValidPhone;
 
@@ -18,11 +19,11 @@ public class RegisterRequest {
     private String password;
 
     @NotBlank(message = "Имя обязательно для заполнения")
-    @Size(min = 2, max = 50, message = "Имя должно быть от 2 до 50 символов")
+    @ValidFirstName
     private String firstName;
 
     @NotBlank(message = "Фамилия обязательна для заполнения")
-    @Size(min = 2, max = 50, message = "Фамилия должна быть от 2 до 50 символов")
+    @ValidLastName
     private String lastName;
 
     @NotBlank(message = "Телефон обязателен для заполнения")
--- src/main/java/test/weewee/userservice/dto/UpdateUserRequest.java
+++ src/main/java/test/weewee/userservice/dto/UpdateUserRequest.java
@@ -1,8 +1,9 @@
 package test.weewee.userservice.dto;
 
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import test.weewee.userservice.validation.ValidEmail;
+import test.weewee.userservice.validation.ValidFirstName;
+import test.weewee.userservice.validation.ValidLastName;
 import test.weewee.userservice.validation.ValidPassword;
 import test.weewee.userservice.validation.ValidPhone;
 
@@ -14,10 +15,10 @@ public class UpdateUserRequest {
     @ValidPassword
     private String password;
 
-    @Size(min = 2, max = 50, message = "Имя должно быть от 2 до 50 символов")
+    @ValidFirstName
     private String firstName;
 
-    @Size(min = 2, max = 50, message = "Фамилия должна быть от 2 до 50 символов")
+    @ValidLastName
     private String lastName;
 
     @ValidPhone
