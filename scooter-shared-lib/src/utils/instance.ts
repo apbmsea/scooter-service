b9ca38291118f715da0c1bc4b9d70d932c174f4b
apbmsea <apbmsea@gmail.com>
2025-12-08 19:32:58 +0500

    fix instance bug, add docs

--- scooter-shared-lib/src/utils/instance.ts
+++ scooter-shared-lib/src/utils/instance.ts
@@ -11,16 +11,16 @@ function getRefreshInstance() {
   if (typeof window !== "undefined" && window.__REFRESH__) {
     return window.__REFRESH__;
   }
-
-  throw new Error("Refresh not init");
+  console.error("Рефреш инстанс работает в дев режиме!")
+  return axios.create({});
 }
 
 function getApiInstance() {
   if (typeof window !== "undefined" && window.__API__) {
     return window.__API__;
   }
-
-  throw new Error("Instance not init");
+  console.error("Инстанс работает в дев режиме!")
+  return axios.create({});
 }
 
 export const $api = getApiInstance();
